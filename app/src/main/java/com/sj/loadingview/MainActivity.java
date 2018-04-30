b973e8a95a3c38594f4e5322491dd4fc1f400b48
shijia109 <1094838711@qq.com>
2018-04-30 23:47:54 +0800

    添加控制

--- app/src/main/java/com/sj/loadingview/MainActivity.java
+++ app/src/main/java/com/sj/loadingview/MainActivity.java
@@ -1,13 +1,44 @@
 package com.sj.loadingview;
 
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
 
-public class MainActivity extends AppCompatActivity {
+import com.sj.library.BallLoadingView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private BallLoadingView loadingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.show).setOnClickListener(this);
+        findViewById(R.id.hide).setOnClickListener(this);
+        loadingView = findViewById(R.id.loadingView);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.show:
+                loadingView.setVisibility(View.VISIBLE);
+                break;
+            case R.id.hide:
+                loadingView.setVisibility(View.GONE);
+                break;
+        }
     }
 }
